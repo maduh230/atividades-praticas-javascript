ae13399773efad3180eba98ab81f8d739de13f69
ana luisa coutinho <mariaeduardamj10@gmail.com>
2026-08-31 13:22:40 -0300

    Create 04-funcoes-dom-1-fila.js

---
+++ 04-funcoes-dom-1-fila.js
@@ -0,0 +1,6 @@
+//função simples: recebe um nome e adiciona um <li> na lista DOM//
+function adicionarNafila(nome){
+    const item = document.createElement('li');
+    item.textContent = nome;
+    ''
+}
